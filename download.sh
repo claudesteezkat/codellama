b8393f9064814c6ad487fb41915f2be1d77b01f9
claudesteezkat <122759797+claudesteezkat@users.noreply.github.com>
2023-08-27 14:04:43 +0200

    commit

--- download.sh
+++ download.sh
@@ -3,9 +3,9 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-read -p "Enter the URL from email: " PRESIGNED_URL
+read -p "https://download2.llamameta.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoieXN0Y2YzaHcxa3UzZDR4NzQ2YXhpYWZoIiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZG93bmxvYWQyLmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2OTMyMDU1OTF9fX1dfQ__&Signature=FNqMs6LaeBOFFHNzpdt6Bn9IV%7EXcdKle46FW50iqHtHf7dFYD-pe6fkPHNGGEa3e25yySr86k62z%7E0UXAsPqNoDdVl7%7Ee%7EFnkIwWLXFENTiFna%7ElnIrj0lh7lO864oFfjP5t-8v2v79JQ9wHAKb7sI6Vufzm3hVId25BGqe3kUGteMMhw5EVINCKuO5lmibbsSS%7E3jNhpAmNnbksqoBH8exOHmNKQ%7EOSJFqv-xZDa-IGrxJRvr6tjISLiSx%7EzR6X2zV1qUyt4fL3PWgxVVsk9wNqcI59-oiu18CDfr-f5hLrUkSj8fX8vTgAni4iJQ-yy648jPbuh4%7EKfJiiCA1Wfw__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=309022791663015: " PRESIGNED_URL
 echo ""
-ALL_MODELS="7b,13b,34b,7b-Python,13b-Python,34b-Python,7b-Instruct,13b-Instruct,34b-Instruct"
+ALL_MODELS="7b-Python,7b-Instruct"
 read -p "Enter the list of models to download without spaces ($ALL_MODELS), or press Enter for all: " MODEL_SIZE
 TARGET_FOLDER="."             # where all files should end up
 mkdir -p ${TARGET_FOLDER}
